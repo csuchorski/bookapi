cefada6f1f6520769de3599fb8bd593769501e8d
csuchorski <cezary.suchorski@wp.pl>
2022-06-27 20:47:13 +0200

    Added basic seeding for books table

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,30 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Bookapi.Repo.insert!(%Bookapi.Library.Book{
+  title: "Mistborn Book 1 - Final Empire",
+  author: "Brandon Sanderson",
+  total_pages: 650,
+  isbn: "9_780_765_311_788",
+  publisher: "Gollancz",
+  published_date: ~D[2006-01-01]
+})
+
+Bookapi.Repo.insert!(%Bookapi.Library.Book{
+  title: "Unsouled - Cradle Book 1",
+  author: "Will Wight",
+  total_pages: 300,
+  isbn: "9_780_989_671_767",
+  publisher: "Hidden Gnome Publishing",
+  published_date: ~D[2017-01-01]
+})
+
+Bookapi.Repo.insert!(%Bookapi.Library.Book{
+  title: "The Fifth Season",
+  author: "N. K. Jemisin",
+  total_pages: 512,
+  isbn: "9_780_316_229_296",
+  publisher: "Orbit",
+  published_date: ~D[2006-01-01]
+})
