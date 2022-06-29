10de2aa80f0c79cf5814c94a4af381186c7566bd
csuchorski <cezary.suchorski@wp.pl>
2022-06-29 17:48:00 +0200

    reverted changes to fix bugs

--- priv/repo/migrations/20220627180750_add_author_field_books.exs
+++
@@ -1,9 +0,0 @@
-defmodule Bookapi.Repo.Migrations.AddAuthorFieldBooks do
-  use Ecto.Migration
-
-  def change do
-    alter table(:books) do
-      add :author, :string
-    end
-  end
-end
--- priv/repo/migrations/20220627183257_modify_type_isbn_books.exs
+++
@@ -1,9 +0,0 @@
-defmodule Bookapi.Repo.Migrations.ModifyTypeIsbnBooks do
-  use Ecto.Migration
-
-  def change do
-    alter table(:books) do
-      modify :isbn, :string
-    end
-  end
-end
